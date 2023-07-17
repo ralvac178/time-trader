b889d91631b741b7ad15c736b44e6cd363ea9cad
ralvac178 <52391112+ralvac178@users.noreply.github.com>
2023-07-17 14:53:30 -0500

    Added random fire particles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
 
     public static bool isDead = false;
 
+    [SerializeField] private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +29,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             animator.SetBool("IsJump", true);
+            rb.AddForce(Vector3.up * 200);
         }
         else if (Input.GetKeyDown(KeyCode.M))
         {
---
+++ Assets/Scripts/randomPosFire.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomPosFire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float maxLengh = 0;
+        float randomOffset = 0f;
+        if (transform.parent.gameObject.name.Equals("platformTSectionFire(Clone)"))
+        {
+            maxLengh = 8f;
+            randomOffset = Random.Range(-maxLengh, maxLengh);
+            if (randomOffset < 2 && randomOffset > -2)
+            {
+                randomOffset += 3;
+            }
+            transform.localPosition += new Vector3(randomOffset, 0, 0);
+        }
+        else
+        {
+            maxLengh = 4f;
+            randomOffset = Random.Range(-maxLengh, maxLengh);
+            transform.localPosition += new Vector3(0,
+                                    0, randomOffset);
+        }
+
+        
+    }
+
+
+}
