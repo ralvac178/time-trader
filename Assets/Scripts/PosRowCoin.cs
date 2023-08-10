fec24f4f5ea16fb7338d465684596fcbe1d686cc
ralvac178 <52391112+ralvac178@users.noreply.github.com>
2023-08-09 21:28:58 -0500

    Added coin platforms, and coins prefabs

---
+++ Assets/Scripts/PosRowCoin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosRowCoin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject parentGameObject = transform.parent.gameObject;
+        //bool canPos = parentGameObject.name.Contains("platformZSplit") ||
+        //        parentGameObject.name.Contains("platformZThin");
+
+        //if (!canPos)
+        //{
+        //    float randomPosX = Random.Range(-1.8f, 1.8f);
+        //    transform.position = new Vector3(randomPosX, transform.position.y, transform.position.z);
+        //}
+    }
+}
